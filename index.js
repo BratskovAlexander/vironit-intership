5d453e733c2c7b8a277ce507f55a0953a58d2a3d
BratskovAlexander <monutor@mail.ru>
2019-10-30 17:21:24 +0300

    added func  get obj.value

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
 const changeLetter = () => {
-    let string = 'Привет';
-    let newString = string.replace(/ив/i, 'aaaa');
-    console.log(newString);
-}
+  let string = "Привет";
+  let newString = string.replace(/ив/i, "aaaa");
+  console.log(newString);
+};
 changeLetter();
+
+const obj = {
+  a: `Hello`,
+  b: {
+    c: `Alex!`,
+    d: `How are you?`,
+    e: {
+      f: `- I'm fine! Thank you!`
+    }
+  }
+};
+
+const func = (obj) => {
+    for (let value in obj) {
+        debugger;
+        if (typeof obj[value] === "object") {
+          func(obj[value]); 
+        } else {
+          console.log(obj[value]);
+        }
+      }
+}
+func(obj);
