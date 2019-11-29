e83d1e704138d7838530e4e97885624b9f18d0fb
BratskovAlexander <monutor@mail.ru>
2019-11-29 18:14:44 +0300

    added swagger express

--- task Users/src/index.js
+++ task Users/src/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const user_router = require("./routers/user-router");
 const city_router = require("./routers/city-router");
 const mongoose = require("mongoose");
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
 const PORT = process.env.PORT || 3000;
 
@@ -19,8 +21,9 @@ mongoose.connect(
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/user", user_router);
-app.use("/city", city_router);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+app.use("/users", user_router);
+app.use("/cities", city_router);
 
 app.listen(PORT, () => {
   console.log(`Server has been started`);
